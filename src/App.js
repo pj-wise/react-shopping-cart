1cf2ffd712f7f227e23c05122daacf906e279755
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-09-09 17:24:42 -0400

    context created for products and addItem

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import ProductContext from "./contexts/ProductContext";
 import CartContext from "./contexts/CartContext";
 
 function App() {
@@ -17,20 +18,22 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <ProductContext.Provider value={{ products, addItem }}>
       <CartContext.Provider value={cart}>
-        <Navigation />
+        <div className='App'>
+          <Navigation />
+
+          {/* Routes */}
+          <Route
+            exact
+            path='/'
+            render={() => <Products products={products} addItem={addItem} />}
+          />
+
+          <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        </div>
       </CartContext.Provider>
-
-      {/* Routes */}
-      <Route
-        exact
-        path='/'
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
-    </div>
+    </ProductContext.Provider>
   );
 }
 
